906dfca5c41053408d70d3bb2d92cadc6ecd05b2
张非凡 <zhangfeifan@zhangfeifandeMacBook-Pro.local>
2020-09-10 21:42:28 +0800

    终于能用了

---
+++ src/components/MyOwn.vue
@@ -0,0 +1,24 @@
+<template>
+<v-container class="text-center" id="my-app">
+    <v-btn dark elevation="24" color="deep-purple" @click="myfunc">戳我</v-btn>
+    <v-card elevation="24" class="pt-0 mt-7" color="pink">
+        <v-card-text>{{ message }}</v-card-text>
+    </v-card>
+</v-container>
+</template>
+
+<script>
+export default {
+    name: "MyOwn",
+    data: () => ({
+        loading: false,
+        selection: 1,
+        message: "啥几把玩意"
+    }),
+    methods: {
+        myfunc() {
+            this.message = "就是这个几把玩意！"
+        },
+    },
+};
+</script>
